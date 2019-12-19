1196a1c46604b7c6b17017bf57de1046b54d555e
tallsamurai <somigliluca@gmail.com>
2019-12-19 17:10:27 +0100

    Added flask starter app for integration

---
+++ dash_app.py
@@ -0,0 +1,85 @@
+import dash
+import charts
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+import codecs
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SKETCHY])
+
+nav = dbc.Nav([
+    dbc.NavItem((dbc.NavLink("Macro-Analyst", active=True, href="#"))),
+    dbc.NavItem(dbc.NavLink("Home", active=True, href="#")),
+    dbc.NavItem(dbc.NavLink("About", href="#")),
+    dbc.NavItem(dbc.NavLink("Features", href="#")),
+    dbc.NavItem(dbc.NavLink("Contact", href="#")),
+    dbc.DropdownMenu(
+        [dbc.DropdownMenuItem("United States"),
+         dbc.DropdownMenuItem("United Kindom"),
+         dbc.DropdownMenuItem("Canada"),
+         dbc.DropdownMenuItem("Europe"),
+         ],
+        label="Country",
+        nav=True,
+    ),
+    dbc.DropdownMenu(
+        [dbc.DropdownMenuItem("GDP"),
+         dbc.DropdownMenuItem("Stock Market"),
+         dbc.DropdownMenuItem("Balance Of Payments"),
+         dbc.DropdownMenuItem("Trades"),
+         dbc.DropdownMenuItem("Commodities"),
+         dbc.DropdownMenuItem("COT Report"),
+         dbc.DropdownMenuItem("PMI & NMI"),
+         dbc.DropdownMenuItem("Building Permits"),
+         ],
+        label="Indicator",
+        nav=True,
+    )
+]
+)
+
+body = dbc.Container(
+    [
+        dbc.Row([
+            dbc.Col([
+                html.Div(charts.main_chart),
+                html.Table([
+                    html.Thead(
+                        html.Tr(html.Th("Main Statistics"))),
+                    html.Tbody(
+                        [html.Tr([
+                            html.Td(x),
+                            html.Td(round(charts.main_chart_stats[x], 5))
+                        ]) for x in charts.main_chart_stats]
+                    )
+                ])
+            ]),
+            dbc.Col([
+                html.Div(charts.side_chart),
+                html.Table([
+                    html.Thead(html.Tr(html.Th("% Change Statistics"))),
+                    html.Tbody(
+                        [html.Tr([
+                            html.Td(x),
+                            html.Td(round(charts.side_chart_stats[x], 5))
+                        ]) for x in charts.side_chart_stats]
+                    )
+                ])
+            ], width={"size": 6}),
+        ])
+    ])
+
+footer = html.Div([
+    html.P(children="Footer here.")
+])
+
+app.layout = html.Div([
+    nav,
+    body,
+    footer
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8000)
