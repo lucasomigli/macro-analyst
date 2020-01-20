0544066240c8552b63cc18e9d837ace6f6f14755
Trevor McReynolds <trevor.d.mcreynolds@gmail.com>
2020-01-19 22:20:53 -0800

    init SPA function | refactor

--- macroanalyst/dash_app/dashboard.py
+++ macroanalyst/dash_app/dashboard.py
@@ -2,6 +2,7 @@
 #  Dash app
 # =============================
 
+import dash
 from dash import Dash
 from dash.dependencies import Input, Output, State
 import dash_table
@@ -11,6 +12,7 @@ import dash_html_components as html
 import pandas as pd
 
 from .base_layout import html_layout
+from .navbar import navbar
 
 
 def Add_Dash(server):
@@ -40,9 +42,9 @@ def Add_Dash(server):
     # =============================
 
     # Config
-    dash_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-        os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db.sqlite')
-    dash_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # dash_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    #     os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db.sqlite')
+    # dash_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Silence exception to elements not existing until callback execution
     dash_app.config.suppress_callback_exceptions = True
@@ -53,146 +55,123 @@ def Add_Dash(server):
     
     # Override the underlying HTML template
     dash_app.index_string = html_layout
-    
-    PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
-    search_bar = dbc.Row(
-        [
-            html.A(
-                html.H2('Link'),
-                href="/"
-            ),
-            dbc.Col(dbc.NavLink("Page 1", href="/")),
-            dbc.Col(dbc.NavLink("Page 1", href="/")),
-            dbc.Col(dbc.Input(type="search", placeholder="Search")),
-            dbc.Col(
-                dbc.Button("Search", color="secondary"),
-                width="auto",
-            ),
-        ],
-        no_gutters=True,
-        className="ml-auto flex-nowrap mt-3 mt-md-0",
-        align="center",
-    )
+    # body = dbc.Container(
+    #     [
+    #         dbc.Row(
+    #             [
+    #                 dbc.Col(
+    #                     [
+    #                         html.H2("Filters"),
+    #                         dbc.Form([
+    #                             dbc.FormGroup(
+    #                                 [
+    #                                     dbc.Label("Country", html_for="country"),
+    #                                     dcc.Dropdown(
+    #                                         id="country",
+    #                                         options=[
+    #                                             {"label": "Option 1", "value": 1},
+    #                                             {"label": "Option 2", "value": 2},
+    #                                         ],
+    #                                     ),
+    #                                     dbc.Label("Indicator", html_for="indicator"),
+    #                                     dcc.Dropdown(
+    #                                         id="indicator",
+    #                                         options=[
+    #                                             {"label": "Gross Domestic Product", "value": 1},
+    #                                             {"label": "Manufacturing Activity", "value": 2},
+    #                                             {"label": "Inventory Levels", "value": 3},
+    #                                             {"label": "Retail Sales", "value": 4},
+    #                                             {"label": "Building Permits", "value": 5},
+    #                                             {"label": "Housing Market", "value": 6},
+    #                                             {"label": "New Businesses", "value": 7},
+    #                                             {"label": "Income & Wages", "value": 8},
+    #                                             {"label": "Unemployment Rate", "value": 9},
+    #                                             {"label": "Consumer Price Index | Inflation", "value": 10},
+    #                                             {"label": "Currency Strength", "value": 11},
+    #                                             {"label": "Inventory Levels", "value": 12},
+    #                                             {"label": "Interest Rates", "value": 13},
+    #                                             {"label": "Corporate Profits", "value": 14},
+    #                                             {"label": "Balance of Trade", "value": 15},
+    #                                             {"label": "Value of Commodity Substitutes to USD", "value": 16},
+    #                                         ],
+    #                                     )
+    #                                 ],
+    #                                 className="mt-4"
+    #                             )
+    #                         ])       
+    #                     ],
+    #                     lg=4,
+    #                 ),
+    #                 html.Hr(className="my-2"),
+    #                 dbc.Col(
+    #                     [
+    #                         html.H2("Economic Data"),
+    #                         dcc.Graph(
+    #                             figure={"data": [{
+    #                                 "x": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18], 
+    #                                 "y": [1, 4, 9, 11, 26, 24, 25, 32, 45, 67, 110, 112, 134, 123, 127, 113, 120, 135]}
+    #                             ]}
+    #                         ),
+    #                         dbc.FormGroup(
+    #                             [
+    #                                 dbc.Label("RangeSlider", html_for="range-slider"),
+    #                                 dcc.RangeSlider(id="range-slider", min=0, max=1000, value=[400, 700]),
+    #                             ]
+    #                         ),
+    #                     ]
+    #                 ),
+    #             ]
+    #         ),
+    #         html.Hr(className="my-2"),
+    #         dbc.Row(
+    #             [
+    #                 dbc.Col(
+    #                     [
+    #                         html.H2("Heading"),
+    #                         html.P(
+    #                             """This is sample text"""
+    #                         ),
+    #                     ]
+    #                 )
+    #             ]
+    #         )
+    #     ],
+    #     className="mt-4",
+    # )
 
-    navbar = dbc.Navbar(
-        [
-            html.A(
-                # Use row and col to control vertical alignment of logo / brand
-                dbc.Row(
-                    [
-                        dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
-                        dbc.Col(dbc.NavbarBrand("MacroAnalyst", className="ml-2")),
-                    ],
-                    align="center",
-                    no_gutters=True,
-                ),
-                href="/",
-            ),
-            dbc.NavbarToggler(id="navbar-toggler"),
-            dbc.Collapse(search_bar, id="navbar-collapse", navbar=True),
-        ],
-        color="dark",
-        dark=True,
-    )
-
-    body = dbc.Container(
-        [
-            dbc.Row(
-                [
-                    dbc.Col(
-                        [
-                            html.H2("Filters"),
-                            dbc.Form([
-                                dbc.FormGroup(
-                                    [
-                                        dbc.Label("Country", html_for="country"),
-                                        dcc.Dropdown(
-                                            id="country",
-                                            options=[
-                                                {"label": "Option 1", "value": 1},
-                                                {"label": "Option 2", "value": 2},
-                                            ],
-                                        ),
-                                        dbc.Label("Indicator", html_for="indicator"),
-                                        dcc.Dropdown(
-                                            id="indicator",
-                                            options=[
-                                                {"label": "Gross Domestic Product", "value": 1},
-                                                {"label": "Manufacturing Activity", "value": 2},
-                                                {"label": "Inventory Levels", "value": 3},
-                                                {"label": "Retail Sales", "value": 4},
-                                                {"label": "Building Permits", "value": 5},
-                                                {"label": "Housing Market", "value": 6},
-                                                {"label": "New Businesses", "value": 7},
-                                                {"label": "Income & Wages", "value": 8},
-                                                {"label": "Unemployment Rate", "value": 9},
-                                                {"label": "Consumer Price Index | Inflation", "value": 10},
-                                                {"label": "Currency Strength", "value": 11},
-                                                {"label": "Inventory Levels", "value": 12},
-                                                {"label": "Interest Rates", "value": 13},
-                                                {"label": "Corporate Profits", "value": 14},
-                                                {"label": "Balance of Trade", "value": 15},
-                                                {"label": "Value of Commodity Substitutes to USD", "value": 16},
-                                            ],
-                                        )
-                                    ],
-                                    className="mt-4"
-                                )
-                            ])       
-                        ],
-                        lg=4,
-                    ),
-                    html.Hr(className="my-2"),
-                    dbc.Col(
-                        [
-                            html.H2("Economic Data"),
-                            dcc.Graph(
-                                figure={"data": [{
-                                    "x": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18], 
-                                    "y": [1, 4, 9, 11, 26, 24, 25, 32, 45, 67, 110, 112, 134, 123, 127, 113, 120, 135]}
-                                ]}
-                            ),
-                            dbc.FormGroup(
-                                [
-                                    dbc.Label("RangeSlider", html_for="range-slider"),
-                                    dcc.RangeSlider(id="range-slider", min=0, max=1000, value=[400, 700]),
-                                ]
-                            ),
-                        ]
-                    ),
-                ]
-            ),
-            html.Hr(className="my-2"),
-            dbc.Row(
-                [
-                    dbc.Col(
-                        [
-                            html.H2("Heading"),
-                            html.P(
-                                """This is sample text"""
-                            ),
-                        ]
-                    )
-                ]
-            )
-        ],
-        className="mt-4",
-    )
-
-
-
-
-    dash_app.layout = html.Div([navbar, body])
-
-    # Initialize callbacks after the application is loaded
+    # Initialize callbacks before the application has finished loading
     init_callbacks(dash_app)
+
+    dash_app.layout = html.Div([
+        # Represents the URL bar. Doesn't render anything
+
+        dcc.Location(id='url', refresh=False),
+
+        # The following renders in every case
+        navbar,
+        dcc.Link('Navigate to "/"', href='/'),
+        html.Br(),
+        dcc.Link('Navigate to "/page-2"', href='/page-2'),
+
+        # Content is rendered in this element
+        html.Div(id='page-content')
+    ])
 
     # Launch Application
     return dash_app.server
 
 
 def init_callbacks(dash_app):
+    @dash_app.callback(dash.dependencies.Output('page-content', 'children'),
+                        [dash.dependencies.Input('url', 'pathname')])
+    
+    def display_page(pathname):
+        return html.Div([
+            html.H3('You are on page {}'.format(pathname))
+        ])
+
+    # Pertains to navbar collapse on small viewports
     @dash_app.callback(
         Output("navbar-collapse", "is_open"),
         [Input("navbar-toggler", "n_clicks")],
@@ -204,7 +183,6 @@ def init_callbacks(dash_app):
             return not is_open
         return is_open
     
-    @dash_app.callback
 
 #    def get_datasets():
 #     """Return previews of all CSVs saved in /data directory."""
---
+++ macroanalyst/dash_app/navbar.py
@@ -0,0 +1,46 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+
+PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
+
+search_bar = dbc.Row(
+        [
+            html.A(
+                html.H2('Link'),
+                href="/"
+            ),
+            dbc.Col(dbc.NavLink("Page 1", href="/")),
+            dbc.Col(dbc.NavLink("Page 1", href="/")),
+            dbc.Col(dbc.Input(type="search", placeholder="Search")),
+            dbc.Col(
+                dbc.Button("Search", color="secondary"),
+                width="auto",
+            ),
+        ],
+        no_gutters=True,
+        className="ml-auto flex-nowrap mt-3 mt-md-0",
+        align="center",
+    )
+
+navbar = dbc.Navbar(
+    [
+        html.A(
+            # Use row and col to control vertical alignment of logo / brand
+            dbc.Row(
+                [
+                    dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
+                    dbc.Col(dbc.NavbarBrand("MacroAnalyst", className="ml-2")),
+                ],
+                align="center",
+                no_gutters=True,
+            ),
+            href="/",
+        ),
+        dbc.NavbarToggler(id="navbar-toggler"),
+        dbc.Collapse(search_bar, id="navbar-collapse", navbar=True),
+    ],
+    color="dark",
+    dark=True,
+)
